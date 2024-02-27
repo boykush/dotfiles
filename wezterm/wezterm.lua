3121821c331a8e6533f050b39efcb659b1a47013
Taichi Kushiro <k.taichi0315@gmail.com>
2024-02-27 23:38:21 +0900

    fix wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,16 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'nord'
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+  left = 16,
+  right = 16,
+  top = 8,
+  bottom = 8,
+}
+
 config.font = wezterm.font 'Hack Nerd Font Mono'
 
 return config
